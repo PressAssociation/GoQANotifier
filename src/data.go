7fc9948d3ae722b52406c7937f0f502a2711d388
Niall Bunting <niall.bunting@pressassociation.com>
2019-03-12 15:44:24 +0000

    Use gofmt this cleans up the code
    gofmt is a command that fixes style issues.

--- src/data.go
+++ src/data.go
@@ -1,140 +1,137 @@
 package main
 
 import (
-    "time"
-    "os"
-    "encoding/csv"
-    "log"
+	"encoding/csv"
+	"log"
+	"os"
+	"time"
 )
 
 type Ping struct {
-    ID    string      `json:"service,omitempty"`
-    Time  time.Time   `json:"last,omitempty"`
-    Passed bool    `json:"passed,omitempty"`
+	ID     string    `json:"service,omitempty"`
+	Time   time.Time `json:"last,omitempty"`
+	Passed bool      `json:"passed,omitempty"`
 }
 
 type Service struct {
-    ID    string      `json:"service,omitempty"`
-    Env   string      `json:"env,omitempty"`
-    LastPass *Ping      `json:"lastpass,omitempty"`
-    LastFail *Ping      `json:"lastfail,omitempty"`
+	ID       string `json:"service,omitempty"`
+	Env      string `json:"env,omitempty"`
+	LastPass *Ping  `json:"lastpass,omitempty"`
+	LastFail *Ping  `json:"lastfail,omitempty"`
 }
 
 var services []Service
+
 const FILENAME string = "services.csv"
 
 func setupServices() {
-    services = make([]Service, 0)
-    readFromfile()
+	services = make([]Service, 0)
+	readFromfile()
 }
 
 func getServices() []Service {
-  return services
+	return services
 }
 
 func getService(id string, env string) *Service {
-
-    for i := 0; i < len(getServices()); i++ {
-      attr := &getServices()[i]
-      if attr.ID == id && attr.Env == env {
-        return attr
-      }
-    }
-
-    return new(Service)
+	for i := 0; i < len(getServices()); i++ {
+		attr := &getServices()[i]
+		if attr.ID == id && attr.Env == env {
+			return attr
+		}
+	}
+	return new(Service)
 }
 
 func createPing(id string, passed bool) *Ping {
-    var ping Ping
-    ping.ID = id
-    ping.Time = time.Now()
-    ping.Passed = passed
-    return &ping;
+	var ping Ping
+	ping.ID = id
+	ping.Time = time.Now()
+	ping.Passed = passed
+	return &ping
 }
 
 func createService(id string, env string) *Service {
+	service := Service{ID: id, Env: env}
+	services = append(services, service)
 
-    service := Service{ID: id, Env: env}
-    services = append(services, service)
-
-    return &services[len(services)-1]
+	return &services[len(services)-1]
 }
 
 func updateService(service *Service, ping *Ping, hasPassedTest bool) {
-    if(hasPassedTest){
-      service.LastPass = ping;
-    } else {
-      service.LastFail = ping;
-    }
+	if hasPassedTest {
+		service.LastPass = ping
+	} else {
+		service.LastFail = ping
+	}
 
-    writeToFile()
+	writeToFile()
 }
 
-
 func writeToFile() {
-    file, err := os.Create(FILENAME)
-    if err != nil {
-      log.Print("Cannot create file", err)
-      return
-    }
-    defer file.Close()
+	file, err := os.Create(FILENAME)
+	if err != nil {
+		log.Print("Cannot create file", err)
+		return
+	}
+	defer file.Close()
 
-    writer := csv.NewWriter(file)
-    defer writer.Flush()
+	writer := csv.NewWriter(file)
+	defer writer.Flush()
 
-    for _, service := range getServices() {
-        lastPass := ""
-        lastFail := ""
-        if (service.LastPass != nil) {
-          lastPass = service.LastPass.Time.Format(time.RFC3339)
-        }
-        if (service.LastFail != nil) {
-          lastFail = service.LastFail.Time.Format(time.RFC3339)
-        }
+	for _, service := range getServices() {
+		lastPass := ""
+		lastFail := ""
+		if service.LastPass != nil {
+			lastPass = service.LastPass.Time.Format(time.RFC3339)
+		}
+		if service.LastFail != nil {
+			lastFail = service.LastFail.Time.Format(time.RFC3339)
+		}
 
-        csvline := []string{service.Env, service.ID, lastPass, lastFail}
+		csvline := []string{service.Env, service.ID, lastPass, lastFail}
 
-        err := writer.Write(csvline)
-        if err != nil {
-          log.Print("Cannot write to file", err)
-          return
-        }
-    }
+		err := writer.Write(csvline)
+		if err != nil {
+			log.Print("Cannot write to file", err)
+			return
+		}
+	}
 }
 
 func readFromfile() {
-    // Open CSV file
-    f, err := os.Open(FILENAME)
-    if err != nil {
-        panic(err)
-    }
-    defer f.Close()
+	// Open CSV file
+	f, err := os.Open(FILENAME)
+	if err != nil {
+		panic(err)
+	}
+	defer f.Close()
 
-    // Read File into a Variable
-    lines, err := csv.NewReader(f).ReadAll()
-    if err != nil {
-        panic(err)
-    }
+	// Read File into a Variable
+	lines, err := csv.NewReader(f).ReadAll()
+	if err != nil {
+		panic(err)
+	}
 
-    // Loop through lines & turn into object
-    for _, line := range lines {
+	// Loop through lines & turn into object
+	for _, line := range lines {
 
-        var passPing *Ping
-        if(line[2] != "") {
-          passPing = createPing(line[1], true)
-          passPing.Time, _ = time.Parse(time.RFC3339, line[2])
-        }
+		var passPing *Ping
+		if line[2] != "" {
+			passPing = createPing(line[1], true)
+			passPing.Time, _ = time.Parse(time.RFC3339, line[2])
+		}
 
-        var failPing *Ping
-        if(line[3] != "") {
-          failPing = createPing(line[1], false)
-          failPing.Time, _ = time.Parse(time.RFC3339, line[3])
-        }
+		var failPing *Ping
+		if line[3] != "" {
+			failPing = createPing(line[1], false)
+			failPing.Time, _ = time.Parse(time.RFC3339, line[3])
+		}
 
-        service := createService(line[1], line[0])
-        service.LastPass = passPing
-        service.LastFail = failPing
-    }
+		service := createService(line[1], line[0])
+		service.LastPass = passPing
+		service.LastFail = failPing
+	}
 
-    log.Print("Finished Reading In Data")
+	log.Print("Finished Reading In Data")
 }
--- src/main.go
+++ src/main.go
@@ -1,116 +1,115 @@
 package main
 
 import (
-    "encoding/json"
-    "log"
-    "net/http"
-    "github.com/gorilla/mux"
-    "github.com/fogleman/gg"
-    "bytes"
-    "strconv"
+	"bytes"
+	"encoding/json"
+	"github.com/fogleman/gg"
+	"github.com/gorilla/mux"
+	"log"
+	"net/http"
+	"strconv"
 )
 
 func main() {
-    setupServices()
+	setupServices()
 
-    router := mux.NewRouter()
-    router.HandleFunc("/ping", GetAll).Methods("GET")
-    router.HandleFunc("/ping/{env}", GetEnv).Methods("GET")
-    router.HandleFunc("/ping/{env}/{id}.png", GetImage).Methods("GET")
-    router.HandleFunc("/ping/{env}/{id}", GetPing).Methods("GET")
-    router.HandleFunc("/ping/{env}/{id}", CreatePing).Methods("POST")
-    log.Fatal(http.ListenAndServe(":8000", router))
+	router := mux.NewRouter()
+	router.HandleFunc("/ping", GetAll).Methods("GET")
+	router.HandleFunc("/ping/{env}", GetEnv).Methods("GET")
+	router.HandleFunc("/ping/{env}/{id}.png", GetImage).Methods("GET")
+	router.HandleFunc("/ping/{env}/{id}", GetPing).Methods("GET")
+	router.HandleFunc("/ping/{env}/{id}", CreatePing).Methods("POST")
+	log.Fatal(http.ListenAndServe(":8000", router))
 }
 
 func GetAll(w http.ResponseWriter, r *http.Request) {
-    json.NewEncoder(w).Encode(getServices())
+	json.NewEncoder(w).Encode(getServices())
 }
 
 func GetEnv(w http.ResponseWriter, r *http.Request) {
-    envservices := make([]Service, 0)
+	envservices := make([]Service, 0)
 
-    params := mux.Vars(r)
-    for _, item := range getServices() {
-        if item.Env == params["env"] {
-          envservices = append(envservices, item)
-        }
-    }
+	params := mux.Vars(r)
+	for _, item := range getServices() {
+		if item.Env == params["env"] {
+			envservices = append(envservices, item)
+		}
+	}
 
-    json.NewEncoder(w).Encode(envservices)
+	json.NewEncoder(w).Encode(envservices)
 }
 
-
 func GetPing(w http.ResponseWriter, r *http.Request) {
-    params := mux.Vars(r)
-    json.NewEncoder(w).Encode(getService(params["id"], params["env"]));
+	params := mux.Vars(r)
+	json.NewEncoder(w).Encode(getService(params["id"], params["env"]))
 }
 
 func GetImage(w http.ResponseWriter, r *http.Request) {
-    params := mux.Vars(r)
+	params := mux.Vars(r)
 
-    item := *getService(params["id"], params["env"])
-    if item != (Service{}) {
-         const X = 320
-         const Y = 30
+	item := *getService(params["id"], params["env"])
+	if item != (Service{}) {
+		const X = 320
+		const Y = 30
 
-         dc := gg.NewContext(X, Y)
-         dc.SetRGB(1, 1, 1)
-         dc.Clear()
-         dc.SetRGB(0, 0, 0)
+		dc := gg.NewContext(X, Y)
+		dc.SetRGB(1, 1, 1)
+		dc.Clear()
+		dc.SetRGB(0, 0, 0)
 
-         if (item.LastPass != nil) {
-         lastPass, _ := json.Marshal(item.LastPass.Time)
-         dc.DrawStringAnchored(string(item.Env) + " Last Pass: " + string(lastPass), 2, Y/4, 0, 0.5)
-         }
+		if item.LastPass != nil {
+			lastPass, _ := json.Marshal(item.LastPass.Time)
+			dc.DrawStringAnchored(string(item.Env)+" Last Pass: "+string(lastPass), 2, Y/4, 0, 0.5)
+		}
 
-         if (item.LastFail != nil) {
-         lastFail, _ := json.Marshal(item.LastFail.Time)
-         dc.DrawStringAnchored(string(item.Env) + " Last Fail: " + string(lastFail), 2, Y/4 * 3, 0, 0.5)
-         }
+		if item.LastFail != nil {
+			lastFail, _ := json.Marshal(item.LastFail.Time)
+			dc.DrawStringAnchored(string(item.Env)+" Last Fail: "+string(lastFail), 2, Y/4*3, 0, 0.5)
+		}
 
-         buffer := new(bytes.Buffer)
-         if err := dc.EncodePNG(buffer); err != nil {
-             log.Print("unable to encode image.")
-             http.Error(w, "Unable to encode image.", http.StatusInternalServerError)
-         }
+		buffer := new(bytes.Buffer)
+		if err := dc.EncodePNG(buffer); err != nil {
+			log.Print("unable to encode image.")
+			http.Error(w, "Unable to encode image.", http.StatusInternalServerError)
+		}
 
-         w.Header().Set("Content-Type", "image/jpeg")
-         w.Header().Set("Content-Length", strconv.Itoa(len(buffer.Bytes())))
-         if _, err := w.Write(buffer.Bytes()); err != nil {
-             log.Println("unable to write image.")
-             http.Error(w, "Unable to write image.", http.StatusInternalServerError)
-         }
-    } else {
-      http.Error(w, "Not Found", http.StatusNotFound)
-    }
+		w.Header().Set("Content-Type", "image/png")
+		w.Header().Set("Content-Length", strconv.Itoa(len(buffer.Bytes())))
+		if _, err := w.Write(buffer.Bytes()); err != nil {
+			log.Println("unable to write image.")
+			http.Error(w, "Unable to write image.", http.StatusInternalServerError)
+		}
+	} else {
+		http.Error(w, "Not Found", http.StatusNotFound)
+	}
 }
 
 func CreatePing(w http.ResponseWriter, r *http.Request) {
-    type jsonbody struct {
-        Passed bool `json:"passed"`
-    }
+	type jsonbody struct {
+		Passed bool `json:"passed"`
+	}
 
-    params := mux.Vars(r)
-    decoder := json.NewDecoder(r.Body) 
-    var t jsonbody
-    err := decoder.Decode(&t)
-    if err != nil {
-        //Need a body
-        log.Print("Error decoding json.")
-        http.Error(w, "Bad Request", http.StatusBadRequest)
-        return
-    }
+	params := mux.Vars(r)
+	decoder := json.NewDecoder(r.Body)
+	var t jsonbody
+	err := decoder.Decode(&t)
+	if err != nil {
+		//Need a body
+		log.Print("Error decoding json.")
+		http.Error(w, "Bad Request", http.StatusBadRequest)
+		return
+	}
 
-    ping := createPing(params["id"], t.Passed)
-    attr := getService(params["id"], params["env"]);
+	ping := createPing(params["id"], t.Passed)
+	attr := getService(params["id"], params["env"])
 
-    log.Printf("%+v - Env: %s Service: %s.", *ping, params["env"], params["id"]);
+	log.Printf("%+v - Env: %s Service: %s.", *ping, params["env"], params["id"])
 
-    if *attr == (Service{}) {
-      attr = createService(params["id"], params["env"])
-    }
+	if *attr == (Service{}) {
+		attr = createService(params["id"], params["env"])
+	}
 
-    updateService(attr, ping, t.Passed)
+	updateService(attr, ping, t.Passed)
 
-    json.NewEncoder(w).Encode(attr)
+	json.NewEncoder(w).Encode(attr)
 }
