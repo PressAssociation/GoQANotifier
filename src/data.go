6265274a3e4531e34d12eebb3fc752f31d7bbcdc
Niall Bunting <niall.bunting@pressassociation.com>
2019-03-12 15:02:12 +0000

    Store the data in a CSV
    This allows the server to be taken down and then brought back up
    with all the stored data that it previously used.

--- src/data.go
+++ src/data.go
@@ -2,6 +2,9 @@ package main
 
 import (
     "time"
+    "os"
+    "encoding/csv"
+    "log"
 )
 
 type Ping struct {
@@ -18,9 +21,11 @@ type Service struct {
 }
 
 var services []Service
+const FILENAME string = "services.csv"
 
 func setupServices() {
     services = make([]Service, 0)
+    readFromfile()
 }
 
 func getServices() []Service {
@@ -61,4 +66,75 @@ func updateService(service *Service, ping *Ping, hasPassedTest bool) {
     } else {
       service.LastFail = ping;
     }
+
+    writeToFile()
+}
+
+
+func writeToFile() {
+    file, err := os.Create(FILENAME)
+    if err != nil {
+      log.Print("Cannot create file", err)
+      return
+    }
+    defer file.Close()
+
+    writer := csv.NewWriter(file)
+    defer writer.Flush()
+
+    for _, service := range getServices() {
+        lastPass := ""
+        lastFail := ""
+        if (service.LastPass != nil) {
+          lastPass = service.LastPass.Time.Format(time.RFC3339)
+        }
+        if (service.LastFail != nil) {
+          lastFail = service.LastFail.Time.Format(time.RFC3339)
+        }
+
+        csvline := []string{service.Env, service.ID, lastPass, lastFail}
+
+        err := writer.Write(csvline)
+        if err != nil {
+          log.Print("Cannot write to file", err)
+          return
+        }
+    }
+}
+
+func readFromfile() {
+    // Open CSV file
+    f, err := os.Open(FILENAME)
+    if err != nil {
+        panic(err)
+    }
+    defer f.Close()
+
+    // Read File into a Variable
+    lines, err := csv.NewReader(f).ReadAll()
+    if err != nil {
+        panic(err)
+    }
+
+    // Loop through lines & turn into object
+    for _, line := range lines {
+
+        var passPing *Ping
+        if(line[2] != "") {
+          passPing = createPing(line[1], true)
+          passPing.Time, _ = time.Parse(time.RFC3339, line[2])
+        }
+
+        var failPing *Ping
+        if(line[3] != "") {
+          failPing = createPing(line[1], false)
+          failPing.Time, _ = time.Parse(time.RFC3339, line[3])
+        }
+
+        service := createService(line[1], line[0])
+        service.LastPass = passPing
+        service.LastFail = failPing
+    }
+
+    log.Print("Finished Reading In Data")
 }
